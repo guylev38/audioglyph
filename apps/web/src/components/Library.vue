617b9e67a658e7fa80d54953cda97d76d3af5005
guylev38 <guydovlev@gmail.com>
2026-07-31 19:31:37 +0300

    Created 3 dots menu for each book

--- apps/web/src/components/Library.vue
+++ apps/web/src/components/Library.vue
@@ -1,51 +1,18 @@
 <script lang="ts" setup>
-const items = [
+const books = [
   {
     title: "Book 1",
     value: 1,
   },
   {
     title: "Book 2",
-    value: 1,
-  },
-  {
-    title: "Book 3",
-    value: 3,
-  },
-  {
-    title: "Book 3",
-    value: 3,
-  },
-  {
-    title: "Book 3",
-    value: 3,
-  },
-
-  {
-    title: "Book 3",
-    value: 3,
-  },
-
-  {
-    title: "Book 3",
-    value: 3,
-  },
-
-  {
-    title: "Book 3",
-    value: 3,
-  },
-
-  {
-    title: "Book 3",
-    value: 3,
+    value: 2,
   },
 ];
 
 const bookOptions = [
-  { title: "edit", prependIcon: "mdi-edit", code: "edit" },
-  { type: "divider" },
-  { title: "Remove", prependIcon: "mdi-trash-can", code: "delete" },
+  { title: "Edit", prependIcon: "mdi-pencil", code: "book-edit" },
+  { title: "Remove", prependIcon: "mdi-trash-can", code: "book-delete" },
 ];
 </script>
 
@@ -55,25 +22,41 @@ const bookOptions = [
       style="display: flex; flex-direction: row; justify-content: space-between"
     >
       <h2>Your Books</h2>
-      <v-btn>
+      <v-btn color="green-darken-1">
         <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-container>
     <v-list>
-      <v-list-item v-for="item in items" :key="item.title" :title="item.title">
+      <v-list-item
+        v-for="book in books"
+        :key="book.title"
+        :title="book.title"
+        style="padding: 10px"
+      >
         <template v-slot:prepend>
-          <v-avatar color="blue-lighten-1">
+          <v-avatar color="blue-darken-1">
             <v-icon color="white"> mdi-book </v-icon>
           </v-avatar>
         </template>
-
         <template v-slot:append>
-          <v-menu>
+          <v-menu location="bottom">
             <template v-slot:activator="{ props }">
-              <v-btn>
+              <v-btn v-bind="props">
                 <v-icon> mdi-dots-vertical </v-icon>
               </v-btn>
             </template>
+            <v-list>
+              <v-list-item
+                v-for="(option, index) in bookOptions"
+                :key="index"
+                :value="index"
+              >
+                <v-list-item-title style="font-size: small">
+                  <v-icon size="20px"> {{ option.prependIcon }}</v-icon>
+                  {{ option.title }}
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
           </v-menu>
         </template>
       </v-list-item>
